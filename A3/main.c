114fddf5228e23661d80dd257e14d9b69c92baa9
Yujie Miao <emolangshen@gmail.com>
2018-11-06 11:31:08 +0800

    testing

--- A3/main.c
+++ A3/main.c
@@ -6,60 +6,7 @@
 #include <ctype.h>
 
 #include "freq_list.h"
-#include "worker.h"
-
-char *remove_punc(char *);
-
-/* Returns a pointer to a linked list of nodes where each node
-* contains a word and the count of the number of occurrences of the word.
-*/
-Node *index_file(Node *head, char *fname, char **filenames) {
-    char line[MAXLINE];
-    char *marker, *token;
-    int countlines = 0;
-    FILE *fp;
-
-    if ((fp = fopen(fname, "r")) == NULL) {
-        perror(fname);
-        exit(1);
-    }
-
-    while ((fgets(line, MAXLINE, fp)) != NULL) {
-        countlines++;
-        if ((countlines % 1000) == 0) {
-            printf("processed %d lines from %s (words%d)\n", countlines, fname, num_words);
-        }
-
-        if (strlen(line) > 0 ) {
-            line[strlen(line) - 1] = '\0';
-        } else {
-            continue;
-        }
-
-        if (strlen(line) == 0) {
-            continue;
-        }
-
-        marker = line;
-        while ((token = strsep(&marker, " \t")) != NULL) {
-            if (strlen(token) == 0) {
-                continue;
-            }
-
-            token = remove_punc(token);
-            if ((strlen(token) <= 3) || isdigit(*token)) {
-                free(token);
-                continue;
-            }
-
-            if (*token != '\0') {
-                head = add_word(head, filenames, token, fname);
-            }
-            free(token);
-        }
-    }
-    return head;
-}
+#include "indexer.h"
 void print_freq_records(FreqRecord *frp) {
 		int i = 0;
 		while(frp != NULL && frp[i].freq != 0) {
